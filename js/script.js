e6c71c285a3c5c2ff82dd2462e4974036b65723a
MyUsername <my@email.com>
2022-11-24 15:34:55 +0100

    insert let and const on random number sect

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@
  //Mail
  //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
  //Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+
+ //Per il gioco dei dadi dobbiamo quindi generare un numero random da 1 a 6, sia per il nostro giocatore che per il computer in sè
+
+ //Section numeri randomici 
+
+ let numberRandomUsers = Math.floor(Math.random() * 6)
+ let numberRandomComputer = Math.floor(Math.random() * 6)
+
+ const numberGamers = document.getElementById('numberGamers')
+ const computerNumber = document.getElementById('computerNumber')
+ const risultato = document.getElementById('risultato')
+
+
